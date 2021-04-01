cb828ad02685923449aa3829ba43d82891ec2760
Samar Majid <38479444+samarmajid@users.noreply.github.com>
2021-04-01 03:14:36 -0400

    added comments to script

--- public/script.js
+++ public/script.js
@@ -1,23 +1,38 @@
 /* eslint-disable max-len */
 
 
-  //taking in map 
   async function dataHandler(){
    
     //getting data from the api 
+
+    //fetching data from api 
     const request = await fetch('/api/dining');
+
+    //getting .json values from api data 
     const api_data = await request.json();
+
+    //getting data values from the api data 
     const data = api_data.data;
+
+    //returns the element that matches table 
     const table = document.querySelector('.table');
 
     console.log(data);
 
+    //looping through the data 
     data.forEach(element => {
+      //logging all rhe elements of the data 
       console.log(element.hall_name);
       console.log(element.hall_address);
       console.log(element.hall_id)
+
+      //creating row for table 
       const appendItem = document.createElement('tr');
+
+      //changing this item w the elements of the data 
       appendItem.innerHTML = `<td>${element.hall_id}</td><td>${element.hall_name}</td><td>${element.hall_address}</td>`;
+      
+      //adding all this to the table 
       table.append(appendItem);
       
     });
