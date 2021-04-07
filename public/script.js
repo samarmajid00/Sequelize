29cf1438792339ce12d52fff5dd5569b616da44c
Samar Majid <38479444+samarmajid@users.noreply.github.com>
2021-04-07 11:15:30 -0400

    added chart, read in macro data, and added calorie info to stacked bar chart

--- public/script.js
+++ public/script.js
@@ -37,18 +37,86 @@
       
     });
 
+  }
+
 
 
     
    
 
     //targetList.append(appendItem);
- 
+
+  async function dataHandlerMacros(){
+    //getting data from the api 
+
+    //fetching data from api 
+    const request = await fetch('/api/macros');
+
+    //getting .json values from api data 
+    const api_data = await request.json();
+
+    //getting data values from the api data 
+    const data = api_data.data;
+
+
+    console.log(api_data);
+
+    let macro_data =[
+      {
+        type: "stackedBar",
+        name: "Calories",
+        showInLegend: "true",
+        
+        dataPoints: [
+
+      
+        ]
+      },
+    ]
+
+    //looping through the data 
+    api_data.forEach(element => {
+      //logging all rhe elements of the data 
+      console.log(element.macro_id);
+      console.log(element.calories);
+      console.log(element.sodium)
+
+      macro_data[0].dataPoints.push({ x: element.meal_id, y: element.calories })
+     
+
+      
+    });
+
+
+
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      title:{
+      text: "Meal Macro Information"
+      },
+
+     
+
+      data: macro_data
+
+
+     
+
+      
+    });
+
+    chart.render();
+
   }
+
+
+
+    
   
     async function windowActions(){
     
       await dataHandler();
+      await dataHandlerMacros();
     }
   
   window.onload = windowActions;
