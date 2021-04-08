77275e0ab7148492681cd48bae42d4a111d347c1
Samar Majid <38479444+samarmajid@users.noreply.github.com>
2021-04-08 11:42:33 -0400

    added all macro info to chart, added 10 random meals

--- public/script.js
+++ public/script.js
@@ -1,6 +1,23 @@
 /* eslint-disable max-len */
 
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function get_random_meals(data){
+  let random_meals = []
+  for (i = 0; i < 10; i++){
+    let current_random_meal = getRandomInt(data.length-1);
+    random_meals.push(data[current_random_meal]);
+    data.splice(current_random_meal,1)
+  }
+  return random_meals;
+}
+
+
+
+
   async function dataHandler(){
    
     //getting data from the api 
@@ -17,7 +34,7 @@
     //returns the element that matches table 
     const table = document.querySelector('.table');
 
-    console.log(data);
+   
 
     //looping through the data 
     data.forEach(element => {
@@ -59,33 +76,85 @@
     const data = api_data.data;
 
 
-    console.log(api_data);
 
     let macro_data =[
       {
         type: "stackedBar",
         name: "Calories",
         showInLegend: "true",
-        
-        dataPoints: [
+        dataPoints: []
+      },
+
+      {
+        type: "stackedBar",
+        name: "Serving Size",
+        showInLegend: "true",
+        dataPoints: []
+      },
+      {
+        type: "stackedBar",
+        name: "Cholesterol",
+        showInLegend: "true",
+        dataPoints: []
+      },
+      {
+        type: "stackedBar",
+        name: "Sodium",
+        showInLegend: "true",
+        dataPoints: []
+      },
+      {
+        type: "stackedBar",
+        name: "Carbs",
+        showInLegend: "true",
+        dataPoints: []
+      },
+      {
+        type: "stackedBar",
+        name: "Protein",
+        showInLegend: "true",
+        dataPoints: []
+      },
+      {
+        type: "stackedBar",
+        name: "Fat",
+        showInLegend: "true",
+        dataPoints: []
+      },
+   
 
       
-        ]
-      },
     ]
 
+    let random_meal_list = get_random_meals(api_data);
+
     //looping through the data 
-    api_data.forEach(element => {
-      //logging all rhe elements of the data 
-      console.log(element.macro_id);
-      console.log(element.calories);
-      console.log(element.sodium)
+    for(i =0; i < random_meal_list.length; i++){
+      element = random_meal_list[i]
 
-      macro_data[0].dataPoints.push({ x: element.meal_id, y: element.calories })
-     
+    
 
+
+
+
+      const name_request = await fetch(`/api/meals/${element.meal_id}`);
+
+      //getting .json values from api data 
+      const name_data = await name_request.json();
+
+      console.log(name_data);
+  
+
+      macro_data[0].dataPoints.push({ label: name_data[0].meal_name, y: element.calories })
+      macro_data[1].dataPoints.push({ label: name_data[0].meal_name, y: element.serving_size })
+      macro_data[2].dataPoints.push({ label: name_data[0].meal_name, y: element.cholesterol })
+      macro_data[3].dataPoints.push({ label: name_data[0].meal_name, y: element.sodium })
+      macro_data[4].dataPoints.push({ label: name_data[0].meal_name, y: element.carbs })
+      macro_data[5].dataPoints.push({ label: name_data[0].meal_name, y: element.protein })
+      macro_data[6].dataPoints.push({ label: name_data[0].meal_name, y: element.fat })
       
-    });
+      
+    };
 
 
 
